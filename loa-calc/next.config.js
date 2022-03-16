d3f3129d058f30d9c5ab4fa6f1caa8fcaf7ded7d
LeeKangbok <pmmm114@gmail.com>
2022-03-16 17:21:23 +0900

    Fix: react18 experimental issue
    - react 18 -> react 17 ( latest )

--- loa-calc/next.config.js
+++ loa-calc/next.config.js
@@ -4,18 +4,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   webpack5: true,
   reactStrictMode: true,
-  experimental: {
-    runtime: "nodejs",
-    serverComponents: true,
-  },
   i18n,
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-    removeConsole: {
-      exclude: ["error"],
-    },
-  },
   swcMinify: true,
 };
 
